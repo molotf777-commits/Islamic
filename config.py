f8ee1c8ab12967a11df99bdb96f4401fb9de32af
MOLOTOF <molotf777@gmail.com>
2026-09-09 14:21:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,23 @@
-"""
-config.py - ملف الإعدادات
-"""
-
 import base64
 
 # توكن البوت
 BOT_TOKEN = "PUT_YOUR_BOT_TOKEN_HERE"
 
 # معرفات القنوات
-AZKAR_CHANNEL_ID = 1471987981299749004
-QURAN_CONTROL_CHANNEL_ID = 1471987630399819908
-QURAN_VOICE_CHANNEL_ID = 1471987630399819908
+AZKAR_CHANNEL_ID = 12345678 حط الايدي بتاع الشات بتاع الاذكار
+QURAN_CONTROL_CHANNEL_ID = 12345678 حط الايدي بتاع الفويس 
+QURAN_VOICE_CHANNEL_ID = 12345678 حط الايدي بتاع الفويس
 
 # الفاصل بين الأذكار بالدقائق
 AZKAR_INTERVAL = 60
 
 # الألوان
-COLOR_MAIN = 0xFF0000
+COLOR_MAIN = 0xFF0000 هنا الالوان
 
 # روابط الصور
-AZKAR_IMAGE = "https://cdn.discordapp.com/attachments/1462058133835747381/1543349993082200224/Gemini_Generated_Image_kcocpakcocpakcoc.png?ex=6a948c30&is=6a933ab0&hm=0470ff7a199da81de3a4ce512ba616c7431fe5e1bfd46d1e53091865a71d49e0&"
+AZKAR_IMAGE = "حط الرابط بتاع الصوره الي انت عايزها تتبعت مع الاذكار"
 
-QURAN_IMAGE = "https://cdn.discordapp.com/attachments/1462058133835747381/1544855500066131998/ChatGPT_Image_Sep_3_2026_02_42_59_AM.png?ex=6a9a064d&is=6a98b4cd&hm=9a8b3f482de20c29767b5ca9efacf041a3b511dd90845cd868ca92fc3cbba851&"
+QURAN_IMAGE = "حط الرابط للصوره اللي انت عايزها هنا الصوره الي هتكون موجوده في لوحه التححكم بتاعت القران"
 
 # إعدادات الصوت
 FFMPEG_OPTIONS = {
